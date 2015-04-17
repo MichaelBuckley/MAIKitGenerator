7e9692126fa098d6e2db0159f167146f5abe2658
Michael Buckley <thebuckley@gmail.com>
2015-04-16 19:04:33 -0700

    Add Mac and iOS project files

---
+++ MAIKit-ios/MAIKit-iosTests/MAIKit_iosTests.m
@@ -0,0 +1,40 @@
+//
+//  MAIKit_iosTests.m
+//  MAIKit-iosTests
+//
+//  Created by Buckley on 4/16/15.
+//  Copyright (c) 2015 Buckleyisms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MAIKit_iosTests : XCTestCase
+
+@end
+
+@implementation MAIKit_iosTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
